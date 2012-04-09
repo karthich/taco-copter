e63943026c6be6dcf34b4e237cffdf41a6318a27
pkernoobie@gmail.com <pkernoobie@gmail.com@d8a557f1-6dd9-e039-3740-ed72e3108095>
2012-04-09 19:03:26 +0000

    Updated State2D and Object files

--- tacocopterbase/tacocopterbase/tacocopterbase/Object.cs
+++ tacocopterbase/tacocopterbase/tacocopterbase/Object.cs
@@ -4,69 +4,56 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using WindowsGame1;
 
 namespace tacocopterbase
 {
     class Object : DrawableGameComponent
     {
-        private State2D currentState;
-
-        private Texture2D sprite;
-        private SpriteBatch spriteBatch;
+		protected State2D State { get; set; }
+		protected Texture2D sprite { get; set; }
+		public SpriteBatch spriteBatch { get; set; }
+		private Vector2 offset;
         protected Game thisGame;
 
+		// commented this out because State2D does not
+		// have a default constructor and it was an error
+		/*
         public Object(Game g):base(g)
         { 
             currentState = new State2D();
             thisGame = g;
-        }
+        }*/
 
         public Object(State2D s,Game g) :base(g)
         { 
-            currentState = s;
+            State = s;
             thisGame = g;
          }
 
         public override void Initialize()
         {
             spriteBatch = new SpriteBatch(this.Game.GraphicsDevice);
+			// calculate offset
+			offset = new Vector2(sprite.Height / 2, sprite.Width / 2);
             LoadContent();
             base.Initialize();
         }
+
         public override void Draw(GameTime gametime)
         {
-            Vector2 offset = new Vector2(sprite.Height / 2, sprite.Width / 2);
             spriteBatch.Begin();
-            spriteBatch.Draw(sprite, currentState.Position1, null, Color.White, 0, new Vector2(0, 0), (Single)0.3, SpriteEffects.None, 0);
+            spriteBatch.Draw(sprite, State.Position - offset, null, Color.White, 0, 
+				new Vector2(0, 0), (Single)0.3, SpriteEffects.None, 0);
             spriteBatch.End();
         }
+
         protected override void LoadContent()
         {
             sprite = thisGame.Content.Load<Texture2D>("black_box");
             base.LoadContent();
         }
 
-        public void Update(State2D s)
-        {
-            currentState = s;
-        }
-
-        protected Texture2D Sprite
-        {
-            get { return sprite; }
-            set { sprite = value; }
-        }
-
-        public SpriteBatch Spritebatch
-        {
-            get { return spriteBatch; }
-            set { spriteBatch = value; }
-        }
-
-        protected State2D S0
-        {
-            get { return currentState; }
-            set { currentState = value; }
-        }
+        public void Update(State2D s) { State = s; }
     }
 }
