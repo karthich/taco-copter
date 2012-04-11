f22b3f32126723ae211ae76ae779fac6f0fbc2b0
pkernoobie@gmail.com <pkernoobie@gmail.com@d8a557f1-6dd9-e039-3740-ed72e3108095>
2012-04-11 18:05:02 +0000

    Added missing files - sorry karthic

---
+++ trunk/tacocopterbase/tacocopterbase/tacocopterbase/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using WindowsGame1;
+
+namespace tacocopterbase {
+	class Customer : Object {
+		private Vector2 offset;
+
+		// nothing special about a Customer yet
+		public Customer(State2D s,Game g) : base(s, g) {
+        }
+
+		// load a generic person sprite
+		protected override void LoadContent() {
+			sprite = thisGame.Content.Load<Texture2D>("gb_walk.png");
+			offset = new Vector2(sprite.Height / 2, sprite.Width / 2);
+			base.LoadContent();
+		}
+
+	}
+}
